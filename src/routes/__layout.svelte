ee23407a0494c7de9465b91e1889548be7f996ae
Noel Jacob <noeljacob91@gmail.com>
2022-02-20 13:52:52 +0530

    init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import '../app.css';
+</script>
+
+<nav></nav>
+
+<header></header>
+
+<main>
+  <slot />
+</main>
+
+<footer></footer>
+
+<style>
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script context="module" lang="ts">
+  export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<section>
+  <h1>One giant leap for mankind</h1>
+  <img
+</section>
+
+<style>
+</style>
